238aa5e64940caf2a683c8c0e23c1b2d4bcf1e90
esraaobeido <esraaabeido123@gmail.com>
2023-04-12 22:57:31 +0000

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let myForm =document.getElementById("employeesForm");
 let infoSection = document.getElementById("cardSection");
 let employees = [];
 
-function Employee (fullName, isAdministration , isMarketing ,isDevelopment,isFinance,isJunior,ismidSenior,isSenior, image){
+function Employee (id ,fullName, isAdministration , isMarketing ,isDevelopment,isFinance,isJunior,ismidSenior,isSenior, image){
     this.fullName = fullName;
     this.isAdministration = isAdministration;
     this.isMarketing = isMarketing;
@@ -14,37 +14,13 @@ function Employee (fullName, isAdministration , isMarketing ,isDevelopment,isFin
     this.ismidSenior = ismidSenior;
     this.isSenior = isSenior;
     this.image = image;
-    this.employeeId = 0;
     this.salary = 0;
+    if(id){
+        this.employeeId = id;
+    }else{
+        this.employeeId =0;
+    }
     employees.push(this);
-}
-
-myForm.addEventListener('submit',handleSubmit);
-function handleSubmit(event){
- event.preventDefault();
- let fullName= event.target.employeeName.value;
- let administration= event.target.administration.checked;
- let marketing= event.target.marketing.checked;
- let development= event.target.development.checked;
- let finance= event.target.finance.checked;
- let junior= event.target.junior.checked;
- let midSenior = event.target.midSenior.checked;
- let senior= event.target.senior.checked; 
- let image= event.target.img.value;
-
-let newEmployee = new Employee(fullName, administration, marketing, development, finance, junior, midSenior, senior, image);
-newEmployee.generateEmployeeID();
-if(newEmployee.isJunior == true){
-    newEmployee.Salary("Junior"); 
-}else if(newEmployee.ismidSenior == true){
-    newEmployee.Salary("Mid-Senior"); 
-} else if(newEmployee.isSenior == true){
-    newEmployee.Salary("Senior"); 
-}
-newEmployee.render();
-  
-
-
 }
 Employee.prototype.Salary=function(Level){
     let tax =0.075;
@@ -52,8 +28,7 @@ Employee.prototype.Salary=function(Level){
    let min = 1500;
    let max = 2000;
    let randomSalary = Math.floor(Math.random() * (max - min + 1) + min);
-   this.salary = randomSalary - (randomSalary*tax);
-    
+   this.salary = randomSalary - (randomSalary*tax); 
   }
 
  else if(Level=="Mid-Senior"){
@@ -69,72 +44,91 @@ Employee.prototype.Salary=function(Level){
    let randomSalary = Math.floor(Math.random() * (max - min + 1) + min);
    this.salary = randomSalary - (randomSalary*tax);
  }
- 
+}
+myForm.addEventListener('submit',handleSubmit);
+function handleSubmit(event){
+ event.preventDefault();
+ let fullName= event.target.employeeName.value;
+ let administration= event.target.administration.checked;
+ let marketing= event.target.marketing.checked;
+ let development= event.target.development.checked;
+ let finance= event.target.finance.checked;
+ let junior= event.target.junior.checked;
+ let midSenior = event.target.midSenior.checked;
+ let senior= event.target.senior.checked; 
+ let image= event.target.img.value;
+
+let newEmployee = new Employee("null", fullName, administration, marketing, development, finance, junior, midSenior, senior, image);
+newEmployee.generateEmployeeID();
+
+if(newEmployee.isJunior == true){
+    newEmployee.Salary("Junior"); 
+}else if(newEmployee.ismidSenior == true){
+    newEmployee.Salary("Mid-Senior"); 
+} else if(newEmployee.isSenior == true){
+    newEmployee.Salary("Senior"); 
+}
+newEmployee.render();
 }
 
 Employee.prototype.render = function() {
-
     let level = '';
     let department = '';
     if (this.isJunior) {
         level = 'Junior';
     } else if (this.ismidSenior) {
         level = 'Mid-Senior';
-    } else {
+    } else if ( this.isSenior){
         level = 'Senior';
     }
 
-    if (this.isMarketing) {
+    if (this.isMarketing == true ) {
         department = 'Marketing';
-    } else if (this.isAdministration) {
+    } else if (this.isAdministration == true) {
         department = 'Administration';
-    } else if (this.isFinance) {
-        dpeartment = 'Finance';
-    } else {
+    } else if (this.isFinance == true) {
+        department = 'Finance';
+    } else if (this.isDevelopment == true){
         department = 'Development';
     }
-    
+
     let div1 = document.createElement('div');
     let img = document.createElement('img');
     let div2 = document.createElement('div');
     let h4 = document.createElement('h4');
     let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
     
     div1.classList.add('card')
     div2.classList.add('container')
 
     h4.textContent = `Name: ${this.fullName} - ID: ${this.employeeId}`;
-    p1.textContent = `Department: ${department} - Level: ${level}`
+    p1.textContent = `Department: ${department} - Level: ${level}`;
+    p2.textContent = `${this.salary}`;
 
     img.src = this.image;
-    img.style.width = '300px';
-    img.style.height = '350px';
+    img.style.width = '200px';
+    img.style.height = '250px';
 
     div1.appendChild(img);
     div1.appendChild(div2);
     div2.appendChild(h4);
     div2.appendChild(p1);
-
-    if (this.isMarketing == true) { 
-    }
+    div2.appendChild(p2);
     
-    if (this.isMarketing) {
+    if(this.isMarketing){
         document.getElementById('marketing-dept').append(div1)
-    } else if (this.isAdministration) {
+    }else if(this.isAdministration){
         document.getElementById('admin-dept').append(div1)
-    } else if (this.isFinance) {
-        document.getElementById('finace-dept').append(div1)
-    } else {
+    }else if(this.isFinance){
+        document.getElementById('finance-dept').append(div1)
+    }else{
         document.getElementById('development-dept').append(div1)
     }
 }
 
-
-// 
-// This is the render method before refactoring
-// 
-
-// Employee.prototype.render = function(){
+// // This is the render method before refactoring
+//     Employee.prototype.render = function(){
 //     const imgEl = document.createElement('img');
 //     imgEl.src = this.image;
 //     imgEl.style.width = '140px';
@@ -172,19 +166,40 @@ Employee.prototype.render = function() {
 //         levEl.textContent = "Level: Senior";
 //         salaryEl.textContent = `Salary: ${this.salary}`;
 //     }
-
 //     infoSection.appendChild(levEl);
 //     infoSection.appendChild(salaryEl);
 // }
 
-Employee.prototype.generateEmployeeID = function(){
-        this.employeeId = Math.floor(1000 + Math.random() * 9000);
-        return this.employeeId;
-      }
-
-for(let i = 0; i < employees.length; i++){
-    employees[i].generateEmployeeID();
-    employees[i].render();
+    Employee.prototype.generateEmployeeID = function(id){
+    this.employeeId = Math.floor(1000 + Math.random() * 9000);
+    return this.employeeId;
 }
+
+let emp1 = new Employee(1000, 'Ghazi Samer',true , false, false, false , false, false, true,'./assets/img4.jpg');
+let emp2 = new Employee(1001, 'Lana Ali', false , false, false, true , false, false, true,'./assets/img3.jpg');
+let emp3 = new Employee(1002, 'Tamara Ayoub', false , true, false, false , false, false, true,'./assets/img2.jpg');
+let emp4 = new Employee(1003, 'Safi Walid', true , false, false, false , false, true, false,'./assets/img5.jpg' );
+let emp5 = new Employee(1004, 'Omar Zaid', false , false, true, false , false, false, true,'./assets/img6.jpg' );
+let emp6 = new Employee(1005, 'Rana Saleh', false , false, true, false , true, false, true,'./assets/img7.jpg' );
+let emp7 = new Employee(1006, 'Hadi Ahmad', false , false, false, true , false, true, false,'./assets/img8.jpg' );
+
+let emps = [emp1, emp2, emp3, emp4, emp5, emp6, emp7]
+for (let i = 0; i< emps.length; i++){
+    if(emps[i].isJunior){
+        emps[i].Salary('Junior');
+    } else if (emps[i].ismidSenior){
+        emps[i].Salary('Mid-Senior');
+    } else if (emps[i].isSenior){
+        emps[i].Salary('Senior');
+    }
+    emps[i].render();
+}
+
+
+// for(let i = 0; i < employees.length; i++){
+//     employees[i].generateEmployeeID();
+//     employees[i].render();
+// }
+
 console.log(employees);
   
